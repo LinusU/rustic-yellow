d1528f9087ad4061ef04011f6e673ddb976a18dd
Linus Unnebäck <linus@folkdatorn.se>
2023-07-13 22:59:24 +0200

    🌹 Remove unused cruft in give_pokemon

--- src/game/engine/events/give_pokemon.rs
+++ src/game/engine/events/give_pokemon.rs
@@ -1,8 +1,6 @@
 use crate::{
-    cpu::Cpu, game::engine::items::item_effects::switch_to_non_full_box, save_state::BoxView,
+    cpu::Cpu, game::engine::items::item_effects::switch_to_non_full_box,
 };
-
-const WRAM_BOX_DATA_START: usize = 0x1a7f;
 
 pub fn hook_give_pokemon_next_end(cpu: &mut Cpu) {
     // If the current box is full, switch to a non-full box
